b3cc51c3c4865e80361ff193e69793d66d394967
Hashem Hashem <hashemmm96@gmail.com>
2018-09-12 15:13:14 +0200

    Finished version

--- weather.py
+++ weather.py
@@ -4,21 +4,19 @@ from urllib.request import Request, urlopen
 import urllib.error
 import tempfile
 from io import BytesIO
+import requests
 
-dataUrl = "http://bulk.openweathermap.org/sample/city.list.json.gz"
-apiEndpoint = "api.openweathermap.org/data/2.5/forecast?"
+dataURL = "http://bulk.openweathermap.org/sample/city.list.json.gz"
+apiURL = "http://api.openweathermap.org/data/2.5/weather"
+apiKey = "d4b2007c1194620471443eb3c6fe4c68"
 
-print("What country are you in?")
-country = input("> ")
-print("And what city are you in?")
+print("What city would you like to see the weather for?")
 city = input ("> ")
-
-country = country.lower()
-city = city.lower()
+city = city.casefold() # for case insensitive matching
 
 # fetch list of cities and their IDs
 try: 
-    with urllib.request.urlopen(dataUrl) as response:
+    with urllib.request.urlopen(dataURL) as response:
         with tempfile.TemporaryFile() as tmp:
             tmp = response.read()
 
@@ -34,3 +32,27 @@ except URLError as e:
 # json file is zipped so need to unzip
 with gzip.open(BytesIO(tmp)) as f:
     locations = json.loads(f.read())
+
+# Check locations for city that was given by user
+cityID = 0
+for entry in locations:
+    if entry['name'].casefold() == city:
+        cityID = entry['id']
+
+if cityID == 0:
+    print ("Your city was not found. Program exited")
+    exit()
+
+# API call to get weather, using necessary parameters
+apiCallParams = {'id': cityID, 'APPID': apiKey}
+req = requests.get(apiURL, params=apiCallParams)
+req.raise_for_status() # raise potential errors
+weatherData = req.json()
+
+# Extract relevant information
+temp = round(weatherData['main']['temp']-273.15, 1)
+weather = weatherData['weather'][0]['main'].lower()
+
+# Print weather status
+print("Weather in " + weatherData['name'] + " is " + weather)
+print("Temperature in degrees celsius:", temp)
